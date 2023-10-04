cb70195d842a330fcb7ef82644b7497c043c575f
jllloydd <wayfivet@gmail.com>
2023-10-04 18:38:47 +0800

    testing of PHP arithmetic operators

---
+++ operators.php
@@ -0,0 +1,22 @@
+<?php
+$numone = 45;
+$numtwo = 237;
+$add = $numone + $numtwo;
+$sub = $numone - $numtwo;
+$mul = $numone * $numtwo;
+$div = $numone / $numtwo;
+$mod = $numone % $numtwo;
+$inc = $numone++;
+$dec = $numone--;
+
+print("This is to test all the PHP arithmetic operators. <br> <br>");
+print("The first number is $numone. The second number is $numtwo.");
+print("<br> <br> Addition Result: $add");
+print("<br> <br> Subtraction Result: $sub");
+print("<br> <br> Multiplication Result: $mul");
+print("<br> <br> Division Result: $div");
+print("<br> <br> Modulo Result: $mod");
+print("<br> <br> Increment Result: $inc");
+print("<br> <br> Decrement Result: $dec");
+
+?>
