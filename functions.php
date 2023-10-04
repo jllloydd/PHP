08b65004d8dcf7b00a6be5ec674446e28a6b1bcc
jllloydd <wayfivet@gmail.com>
2023-10-04 18:21:28 +0800

    documentation of testing PHP variables and parameters

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+    $initialMessage = "This is to test the function of squaring two. <br>";
+    print($initialMessage);
+    $num = 2;
+    print("The number to be squared is $num. <br>");
+
+function square($num){
+    $num = $num**2;
+    return $num;
+}
+
+$numsquared = square($num);
+
+print("Using the function, it will be squared to $numsquared");
+?>
